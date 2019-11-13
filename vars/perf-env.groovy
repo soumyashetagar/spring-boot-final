e967e1734fe2b2378956a54f91ce9e2187788277
Dharanidharkrish <55631557+Dharanidharkrish@users.noreply.github.com>
2019-11-13 17:39:31 +0530

    Create perf-env.groovy

---
+++ vars/perf-env.groovy
@@ -0,0 +1,5 @@
+def call(String msg='PERFORMANCE SERVER')
+{
+  echo "${msg}"
+  sh 'ssh -t -t -i /var/lib/jenkins/.ssh/id_rsa ansadmin@172.31.10.200 "ansible-playbook /opt/playbooks/down_perf.yml"'
+}
