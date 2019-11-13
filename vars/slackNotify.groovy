21bd88c569b324be83ff332e70a214e6b19f4102
Dharanidharkrish <55631557+Dharanidharkrish@users.noreply.github.com>
2019-11-13 16:43:47 +0530

    Update slackNotify.groovy

--- vars/slackNotify.groovy
+++ vars/slackNotify.groovy
@@ -27,11 +27,12 @@ def call(String buildStatus = 'STARTED') {
   }
   // Send notifications
  slackSend (color: colorCode, message: summary)
-  emailext
+  emailext(
       to: mailRecipients,
       replyTo: mailRecipients,
       subject: subject,
       body: details,
       recipientProviders: [[$class: 'DevelopersRecipientProvider']]
      //recipientProviders: [[$class: 'RequesterRecipientProvider']]
+    )
 }
