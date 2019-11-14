f07e8b2ad7fe10c593448ef75fea8c2e1ff0254f
Dharanidharkrish <55631557+Dharanidharkrish@users.noreply.github.com>
2019-11-14 12:43:43 +0530

    Create approval.groovy

---
+++ vars/approval.groovy
@@ -0,0 +1,26 @@
+def call(String msg="APPROVAL")
+{
+  echo "${msg}"
+  script {
+      
+        emailext mimeType: 'text/html',
+                 subject: "[Jenkins]${currentBuild.fullDisplayName}",
+                 to: "maheshwar.mannur@gmail.com",
+                 body: '''<a href="http://13.233.151.1:8080/">Click here to approve </a> "${msg}"''' 
+
+        def userInput = input id: 'userInput',
+                              message: 'Let\'s promote?', 
+                              submitterParameter: 'submitter',
+                              submitter: 'Ritwik',
+                              parameters: [
+                                [$class: 'TextParameterDefinition', defaultValue: 'sit', description: 'Environment', name: 'env'],
+                                [$class: 'TextParameterDefinition', defaultValue: 'k8s', description: 'Target', name: 'target']]
+
+        echo ("Env: "+userInput['env'])
+        echo ("Target: "+userInput['target'])
+        echo ("submitted by: "+userInput['submitter'])
+         
+        }
+         }
+         }
+}
