cfc9877af5d2b8543d7facdc10ff73cba60caf71
Dharanidharkrish <55631557+Dharanidharkrish@users.noreply.github.com>
2019-11-14 14:31:55 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,5 +1,4 @@
-def call(String msg = 'NEXUS'){
-  echo "${msg}"
+def call(body){
   withCredentials([usernamePassword(credentialsId: 'nexus-credentials', passwordVariable: 'pass', usernameVariable: 'userId')]) {
          sh   'curl -F file=@target/myWebApp_Test-${BUILD_NUMBER}.war -u $userId:$pass http://ec2-18-224-155-110.us-east-2.compute.amazonaws.com:8081/nexus/content/repositories/devopstraining/Codekillers/myWebApp_Test-${BUILD_NUMBER}.war'
 			  } 
