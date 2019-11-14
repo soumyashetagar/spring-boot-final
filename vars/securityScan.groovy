82ddf6108d155e6ba9e0d5435e4fc669a0e2969e
Dharanidharkrish <55631557+Dharanidharkrish@users.noreply.github.com>
2019-11-14 18:20:54 +0530

    Update securityScan.groovy

--- vars/securityScan.groovy
+++ vars/securityScan.groovy
@@ -1,4 +1,4 @@
 def call(body)
 {
-  appscan application: 'e1f0cff4-84ac-48cd-a454-3b93259a2488', credentials: 'IBMsecurityscan', email: true, failBuild: true, failureConditions: [failure_condition(failureType: 'high', threshold: 200), failure_condition(failureType: 'total', threshold: 200)], name: 'Security Test', scanner: static_analyzer(hasOptions: false, target: '/var/lib/jenkins/workspace/Jarvis5'), type: 'Static Analyzer', wait: true
+  appscan application: '478cf57c-c285-4d5e-aa31-021622bac065', credentials: 'IBM_Mahesh', email: true, failBuild: true, failureConditions: [failure_condition(failureType: 'high', threshold: 500), failure_condition(failureType: 'total', threshold: 1000)], name: 'Security_Test', scanner: static_analyzer(hasOptions: false, target: '/var/lib/jenkins/workspace/Jarvis5'), type: 'Static Analyzer', wait: true
 }
