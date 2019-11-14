0f115eb5b5a18fd9edd22110c24c4208a1b29a3e
Dharanidharkrish <55631557+Dharanidharkrish@users.noreply.github.com>
2019-11-14 14:17:35 +0530

    Create performance.groovy

---
+++ vars/performance.groovy
@@ -0,0 +1,4 @@
+def call(String msg="PERFORMANCE TESTING")
+{
+  blazeMeterTest abortJob: true, credentialsId: 'blaze', getJtl: true, getJunit: true, testId: '7425356.taurus', workspaceId: '390578'
+}
