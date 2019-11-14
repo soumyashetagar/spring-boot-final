e2c2ea3ac448b08225b83046a3322086ee1b647f
Dharanidharkrish <55631557+Dharanidharkrish@users.noreply.github.com>
2019-11-14 17:09:57 +0530

    Update jira.groovy

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -1,7 +1,7 @@
 def call(String msg="STAGE FAILED",String id="JAR-0")
 {
-  //jiraAddComment comment: ${msg}, idOrKey: ${id}, site: 'jira'
-  def testIssue = [fields: [ project: [id: 'JAR'],
+  jiraAddComment comment: "${msg}", idOrKey: "${id}", site: 'jira'
+  //def testIssue = [fields: [ project: [id: 'JAR'],
                            summary: 'New JIRA Created from Jenkins.',
                            description: 'New JIRA Created from Jenkins.',
                            issuetype: [id: '3']]]
